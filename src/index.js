008d332271026967675fd60fd7029ef790d3ad6d
Agustín Gattone <agustingattone@gmail.com>
2022-06-20 21:39:16 -0300

    changes in ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -4,11 +4,4 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>  
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
